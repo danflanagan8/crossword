7dd6a3e1b607c3ef9dab4ed5101330284138c2ad
Dan Flanagan <dan.flanagan@manifest.com>
2019-08-06 14:18:59 -0500

    Add a bunch of info to the square parsing.

--- src/Plugin/Field/FieldFormatter/CrosswordFormatter.php
+++ src/Plugin/Field/FieldFormatter/CrosswordFormatter.php
@@ -52,22 +52,88 @@ class CrosswordFormatter extends FileFormatterBase {
     $after_grid_index = array_search("<REBUS>", $lines) > -1 ? array_search("<REBUS>", $lines) : array_search("<ACROSS>", $lines);
     $number_of_rows = $after_grid_index - $grid_start_index;
     $grid_lines = array_slice($lines, $grid_start_index, $number_of_rows);
+    $clue_index = [
+      'down' => 0,
+      'across' => 0,
+      'numeral' => 1,
+    ];
+    $clue_order = [];
     foreach ($grid_lines as $row_index => $grid_line) {
       $row = [
         '#theme' => 'crossword_grid_row',
         '#content' => [],
       ];
       for ($col_index = 0; $col_index < strlen($grid_line); $col_index++) {
-        $row['#content'][] = [
-          '#theme' => 'crossword_square',
-          '#fill' => $grid_line[$col_index],
-          '#attributes' => [
-            'data-col' => [(string) $col_index],
-            'data-row' => [(string) $row_index],
-            'data-fill' => $grid_line[(string) $col_index],
-            'class' => ['crossword-square'],
-          ],
+        $fill = $grid_line[(string) $col_index];
+        $classes = [
+          'crossword-square',
         ];
+        if ($fill == ".") {
+          $classes[] = 'black';
+          $row['#content'][] = [
+            '#theme' => 'crossword_square',
+            '#fill' => $grid_line[$col_index],
+            '#attributes' => [
+              'data-col' => [(string) $col_index],
+              'data-row' => [(string) $row_index],
+              'data-fill' => $fill,
+              'class' => $classes,
+            ],
+          ];
+        }
+        else {
+          $increment_numeral = FALSE;
+          $numeral = NULL;
+          /**
+           * This will be the first square in an across clue if...
+           * 1. It's the left square or to the right of a black
+           * AND
+           * 2. It's not the right square and the square to its right is not black.
+           */
+          if ($col_index == 0 || $grid_line[$col_index - 1] == ".") {
+            if (isset($grid_line[$col_index + 1]) && $grid_line[$col_index + 1] !== ".") {
+              $clue_index['across']++;
+              $clue_order[] = 'across';
+              $numeral = $clue_index['numeral'];
+              $increment_numeral = TRUE;
+            }
+          }
+
+          /**
+           * This will be the first square in a down clue if...
+           * 1. It's the top square or the below a black
+           * AND
+           * 2. It's not the bottom square and the square below it is not black.
+           */
+          if ($row_index == 0 || $grid_lines[$row_index - 1][$col_index] == ".") {
+            if (isset($grid_lines[$row_index + 1][$col_index]) && $grid_lines[$row_index + 1][$col_index] !== ".") {
+              $clue_index['down']++;
+              $clue_order[] = 'down';
+              $numeral = $clue_index['numeral'];
+              $increment_numeral = TRUE;
+            }
+          }
+
+          if ($increment_numeral) {
+            $clue_index['numeral']++;
+          }
+
+          $row['#content'][] = [
+            '#theme' => 'crossword_square',
+            '#fill' => $grid_line[$col_index],
+            '#attributes' => [
+              'data-col' => [(string) $col_index],
+              'data-row' => [(string) $row_index],
+              'data-clue-index-down' => [$clue_index['down']],
+              'data-clue-index-across' => [$clue_index['across']],
+              'data-numeral' => [$numeral],
+              'data-fill' => $fill,
+              'class' => $classes,
+            ],
+          ];
+
+        }
+
       }
       $grid['#content'][] = $row;
     }
