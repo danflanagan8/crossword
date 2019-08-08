972dbc9f3602eb2d46a8a1e71ac02ba83ebd36b9
Dan Flanagan <dan.flanagan@manifest.com>
2019-08-08 16:17:43 -0500

    Restrict crossword field type to a single item.

--- src/Plugin/Field/FieldType/CrosswordItem.php
+++ src/Plugin/Field/FieldType/CrosswordItem.php
@@ -6,6 +6,8 @@ use Drupal\file\Plugin\Field\FieldType\FileItem;
 
 /**
  * Plugin implementation of the 'crossword' field type.
+ * This is just like the FileItem but with extra validation
+ * and restricted to one item.
  *
  * @FieldType(
  *   id = "crossword",
@@ -15,7 +17,8 @@ use Drupal\file\Plugin\Field\FieldType\FileItem;
  *   default_widget = "file_generic",
  *   default_formatter = "file_default",
  *   list_class = "\Drupal\file\Plugin\Field\FieldType\FileFieldItemList",
- *   constraints = {"ReferenceAccess" = {}, "FileValidation" = {}, "CrosswordFile" = {}}
+ *   constraints = {"ReferenceAccess" = {}, "FileValidation" = {}, "CrosswordFile" = {}},
+ *   cardinality = "1"
  * )
  */
 class CrosswordItem extends FileItem {
