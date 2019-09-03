d2055ea3bb8d437d3b50324095a01fc1aee54fca
Dan Flanagan <dan.flanagan@manifest.com>
2019-09-03 11:35:59 -0500

    Refactor the crossword thumbnail image generation. Move almost all of it to the CrosswordImageFactory in anticipation of reusing that class with a field formatter.

--- modules/crossword_media/src/Plugin/media/Source/Crossword.php
+++ modules/crossword_media/src/Plugin/media/Source/Crossword.php
@@ -12,7 +12,6 @@ use Drupal\Core\Field\FieldTypePluginManagerInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\File\FileSystem;
 
 /**
  * Crossword media source.
@@ -28,11 +27,11 @@ use Drupal\Core\File\FileSystem;
 class Crossword extends File implements ContainerFactoryPluginInterface {
 
   /**
-   * The file system service.
+   * The crossword image factory service.
    *
-   * @var \Drupal\Core\File\FileSystem
+   * @var \Drupal\crossword\CrosswordImageFactory
    */
-   protected $fileSystem;
+   protected $crosswordImageFactory;
 
   /**
    * Constructs a new class instance.
@@ -51,12 +50,12 @@ class Crossword extends File implements ContainerFactoryPluginInterface {
    *   The field type plugin manager service.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory service.
-   * @param \Drupal\Core\File\FileSystem $file_system
+   * @param \Drupal\crossword\CrosswordImageFactory $crossword_image_factory
    *   The file system service.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, EntityFieldManagerInterface $entity_field_manager, FieldTypePluginManagerInterface $field_type_manager, ConfigFactoryInterface $config_factory, FileSystem $file_system) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, EntityFieldManagerInterface $entity_field_manager, FieldTypePluginManagerInterface $field_type_manager, ConfigFactoryInterface $config_factory, CrosswordImageFactory $crossword_image_factory) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $entity_field_manager, $field_type_manager, $config_factory);
-    $this->fileSystem = $file_system;
+    $this->crosswordImageFactory = $crossword_image_factory;
   }
 
   /**
@@ -71,7 +70,7 @@ class Crossword extends File implements ContainerFactoryPluginInterface {
       $container->get('entity_field.manager'),
       $container->get('plugin.manager.field.field_type'),
       $container->get('config.factory'),
-      $container->get('file_system')
+      $container->get('crossword.image_factory')
     );
   }
 
@@ -92,60 +91,7 @@ class Crossword extends File implements ContainerFactoryPluginInterface {
    *   File URI of the thumbnail image or NULL if there is no specific icon.
    */
   protected function getThumbnail(FileInterface $file) {
-
-    $destination_uri = $this->getDestinationURI($file);
-    if (file_exists($destination_uri)) {
-      // Check if the existing thumbnail is older than the file itself.
-      if (filemtime($file->getFileUri()) <= filemtime($destination_uri)) {
-        // The existing thumbnail can be used, nothing to do.
-        return $destination_uri;
-      }
-      else {
-        // Delete the existing but out-of-date thumbnail.
-        $this->fileSystem->delete($destination_uri);
-        image_path_flush($destination_uri);
-      }
-    }
-    if ($this->createThumbnail($file, $destination_uri)) {
-      return $destination_uri;
-    }
-  }
-
-  /**
-   * Gets the destination URI of the file.
-   *
-   * @param \Drupal\file\FileInterface $file
-   *   The file that is being converted.
-   *
-   * @return string
-   *   The destination URI.
-   */
-  protected function getDestinationURI(FileInterface $file) {
-    $output_path = file_default_scheme() . '://crossword';
-    $filename = "{$file->id()}-thumbnail.jpg";
-    return $output_path . '/' . $filename;
-  }
-
-  /**
-   * Creates and saves the thumbnail image.
-   *
-   * @param \Drupal\file\FileInterface $file
-   *   A file entity.
-   * @param string $destination_uri
-   *   The destination URI.
-   *
-   * @return boolean
-   *   Returns true on success, false on failure.
-   */
-  protected function createThumbnail(FileInterface $file, $destination_uri) {
-    $image_factory = new CrosswordImageFactory($file);
-    $image = $image_factory->getThumbnail();
-    ob_start();
-    imagejpeg($image);
-    $image_data = ob_get_clean();
-    $directory = $this->fileSystem->dirname($destination_uri);
-    file_prepare_directory($directory, FILE_CREATE_DIRECTORY);
-    return $this->fileSystem->saveData($image_data, $destination_uri, FILE_EXISTS_REPLACE);
+    return $this->crosswordImageFactory->getThumbnailUri($file);
   }
 
 }
